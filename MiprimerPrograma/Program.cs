1362e26cc21f71f013250c909db677f03c964697
luismiguelh <luismiguelhh01@hotmail.com>
2023-08-23 21:30:00 -0500

    Código de mi primer programa

---
+++ MiprimerPrograma/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Xml;
+public class MiprimerPrograma
+{
+    static void Main(string[] args)
+    {
+        //Enciciado ingresa por pantalla 2 numeros y calcular las 4 operaciones ariméticas con dicho números
+        //imprimir en pantalla el resultado de cada operacion//
+
+        // Declaracion de mis variables (datos de entrada)
+        int num1, num2, suma, resta, producto, cociente;
+
+        // calculos (procesos)
+        Console.Write("Ingrese su primer número:");
+    num1 = Convert .ToInt32(Console.ReadLine());
+
+    Console .Write("Ingrese su segundo número: " ) ;
+    num2 = Convert .ToInt32(Console.ReadLine());
+
+        // calcular
+        suma = num1 + num2;
+        resta = num1 - num2;
+        producto = num1 * num2;
+        cociente = num1 * num2;
+
+        // imprimir los resultadoo ( salida)
+        Console.Write("La suma es:", suma);
+        Console.Write("La resta es:", resta);
+        Console.Write("La producto es:", producto);
+        Console.Write("La cociente es:", cociente);
+
+
+    }
+}
